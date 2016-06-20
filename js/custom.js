85a6148e985b1b08788f35b59a091ff5ee1bce0d
asokolovskiy <sok.anton95@mail.ru>
2016-06-20 19:51:39 +0400

    Initial

---
+++ js/custom.js
@@ -0,0 +1,40 @@
+
+$(function() {
+	$('.slider').owlCarousel({
+		items: 1,
+		nav: false,
+		dots: true,
+		loop: true,
+		navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
+		responsiveClass:true,
+	    responsive:{
+	        0:{
+	            items:1,
+	            nav:false,
+	            dots: true,
+	        },
+	        600:{
+	            items:1,
+	            nav:false,
+	            dots: true,
+	        },
+	        1000:{
+	            items:1,
+	            nav:false,
+	            loop:true,
+	            dots: true,
+	        }
+	    }
+	});
+	$('.slider-about-us').owlCarousel({
+		items: 3,
+		margin: 20,
+		loop: true,
+		dots: true,
+		stagePadding: 10,
+	});
+	$(".toggle").click(function() {
+	  $(this).toggleClass("on");
+	  $(".menu").slideToggle();
+	});
+});
